066870b89cabdb43bcd5dd461a2995981bccb0e0
ericKwon <ericyong95@gmail.com>
2022-09-10 02:15:30 +0900

    Initial Commit

---
+++ wanted_pre_onboarding-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding-iOS
+//
+//  Created by SEUNGYONG KWON on 2022/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
