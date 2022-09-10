47c37860f872bd2ac392079651baf342a585c905
ericKwon <ericyong95@gmail.com>
2022-09-10 16:34:13 +0900

    feat: Navigation Controller 및 DetailViewController 추가

---
+++ wanted_pre_onboarding-iOS/Controllers/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  wanted_pre_onboarding-iOS
+//
+//  Created by SEUNGYONG KWON on 2022/09/10.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
