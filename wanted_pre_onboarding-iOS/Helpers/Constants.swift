3e68e37fa86761da6ae826f753d139e850e22f71
ericKwon <ericyong95@gmail.com>
2022-09-10 20:46:45 +0900

    fix: WeatherApi url 통합

---
+++ wanted_pre_onboarding-iOS/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  wanted_pre_onboarding-iOS
+//
+//  Created by SEUNGYONG KWON on 2022/09/10.
+//
+
+import UIKit
+
+// MARK: - weather api에 관한 name space 생성
+
+public struct WeatherApi {
+    static let requestURL = "https://api.openweathermap.org/data/2.5/weather?lang=kr"
+    private init() {}
+}
+
