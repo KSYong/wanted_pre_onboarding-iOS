b949ad34aaf11e82940fe3b80ff98c47c3c18d6b
ericKwon <ericyong95@gmail.com>
2022-09-10 20:50:42 +0900

    feat: 네트워크 매니저 추가

---
+++ wanted_pre_onboarding-iOS/Networking/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  wanted_pre_onboarding-iOS
+//
+//  Created by SEUNGYONG KWON on 2022/09/10.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case networkingError
+    case dataError
+    case parseError
+}
+
+class NetworkManager {
+    
+}
