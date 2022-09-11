a6a0e9c7c5c4de760db9d255cf65a4d3ac8b6f80
ericKwon <ericyong95@gmail.com>
2022-09-12 04:13:19 +0900

    feat: NetworkManager 싱글톤 패턴 및 url request 메서드 추가

--- wanted_pre_onboarding-iOS/Networking/NetworkManager.swift
+++ wanted_pre_onboarding-iOS/Networking/NetworkManager.swift
@@ -15,4 +15,40 @@ enum NetworkError: Error {
 
 class NetworkManager {
     
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func getWeatherJson(latitude lat: Double, longitude lon: Double) {
+        
+        guard let url = URL(string: "\(WeatherApi.requestURL)&lat=\(lat)&lon=\(lon)&appid=\(Storage.ApiKey)") else {
+            print("Error: url 생성 실패")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard error == nil else {
+                print("Error: url로부터 GET 실패")
+                print(error!)
+                return
+            }
+            
+            guard let safeData = data else {
+                print("Error: data 수신 실패")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+                print("Error: HTTP request 실패")
+                return
+            }
+            
+            print(String(decoding: safeData, as: UTF8.self))
+        }.resume()
+   
+    }
 }
