4c22e9fe0ec71e0c99e788f3033920dc1c2a2fa7
ericKwon <ericyong95@gmail.com>
2022-09-15 16:44:58 +0900

    feat: 에러 확인 및 디버깅 구문 추가

--- wanted_pre_onboarding-iOS/Networking/NetworkManager.swift
+++ wanted_pre_onboarding-iOS/Networking/NetworkManager.swift
@@ -43,21 +43,26 @@ class NetworkManager {
             
             guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
                 print("Error: HTTP request 실패")
+                print(response.debugDescription)
                 return
             }
             
+            print("파싱 전 준비 완료")
+            print("컴플리션 핸들러 실행")
             completionHandler(self.parseJSON(safeData), error)
         }.resume()
     }
     
     func parseJSON(_ detailData: Data) -> DetailData? {
+        print("JSON 파싱 시작")
         do {
             let decoder = JSONDecoder()
             let decodedData = try decoder.decode(DetailData.self, from: detailData)
+            print("JSON 데이터 디코딩 성공")
             return decodedData
-            
         } catch {
             print("Error: JSON 파일 파싱 실패")
+            print(error)
             return nil
         }
     }
